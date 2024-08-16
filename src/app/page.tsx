33601ac40c35645e6f1aed1e829da6e8a448c97c
CaueFer <cauefernandescaetano@gmail.com>
2024-08-16 10:48:06 -0300

    topnav initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,60 @@
 import TopNav from "@/components/topnav";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <TopNav />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <section
+          id="heroSection"
+          className="min-w-screen min-h-screen overflow-clip max-w-7xl pt-[5%]"
+        >
+          <div className="z-10 absolute inset-0">
+            <Image
+              src="/assets/imgs/bg-last.webp"
+              alt="Background Image"
+              layout="fill"
+              objectFit="cover"
+              quality={100} // MAX 100
+              style={{ zIndex: -1 }}
+            />
+            <Image
+              src="/assets/imgs/bg-medium.webp"
+              className="mix-blend-screen glitch-image"
+              alt="Background Image"
+              layout="fill"
+              objectFit="cover"
+              quality={100} // MAX 100
+              style={{ zIndex: 0 }}
+            />
+            <Image
+              src="/assets/imgs/bg-first.webp"
+              alt="Background Image"
+              layout="fill"
+              objectFit="cover"
+              quality={100} // MAX 100
+              style={{ zIndex: 1 }}
+            />
+          </div>
+
+          <div className="flex flex-col gap-4 z-20 w-full text-7xl text-white relative">
+            <h1>
+              ARTES MARCIAIS. <br />
+              MUSCULAÇÃO.
+              <br />É AQUI.
+            </h1>
+            <h2 className="text-gray-200 text-md">
+              Venha treinar conosco no coração de Joinville. Uma comunidade
+              fantástica com treinadores experientes o aguarda. Juntos, vamos
+              trazer à tona o melhor de você.
+            </h2>
+            <button className="inline-block px-5 py-4 bg-black text-sm">
+              VER AULAS E TURMAS
+            </button>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 export default function TopNav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="bg-gray-700/25 backdrop-filter backdrop-blur-sm w-full fixed top-0 flex justify-between items-center px-10 md:px-20 py-4 max-w-5xl">
-      <h1 className="font-kenyan text-5xl">KENYAN</h1>
-      <li className="flex text-md">
-        <ul>SUPORTE</ul>
-        <ul>CURSOS</ul>
-        <ul>PLANOS</ul>
-      </li>
+    <div
+      className={`w-full sticky top-0 transition-colors duration-500 backdrop-filter z-50
+        ${isScrolled ? "bg-black/75  backdrop-blur-sm " : "bg-transparent"}`}
+    >
+      <div className=" max-w-7xl w-full mx-auto flex justify-between items-center px-10 md:px-20 py-4 text-white">
+        <h1 className="font-kenyan text-5xl">KENYAN</h1>
+        <li className="flex gap-3 text-md font-bold">
+          <ul className="p-2">SUPORTE</ul>
+          <ul className="p-2">CURSOS</ul>
+          <ul className="p-2">PLANOS</ul>
+        </li>
+      </div>
     </div>
   );
 }
