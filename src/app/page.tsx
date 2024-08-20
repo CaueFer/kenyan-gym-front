9ff9422eb017a7bb1edd50a38a303fee9a2bf168
CaueFer <cauefernandescaetano@gmail.com>
2024-08-19 22:12:56 -0300

    WIP:  finish hero section and classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,40 @@ import CardsAula from "@/components/cardsAula";
 import Image from "next/image";
 import { useState } from "react";
 import DefaultBtn from "@/components/defaultBtn";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 
 export default function Home() {
   return (
     <>
       <TopNav />
-      <main className="flex min-w-screen flex-col items-center justify-between px-12 lg:px-24 ">
+      <main className="flex min-w-screen flex-col items-center justify-between">
         <section
           id="heroSection"
-          className="min-w-full min-h-screen pt-[10vh] md:pt-[25vh] "
+          className="min-w-full min-h-screen pt-[10vh] md:pt-[25vh] px-12 lg:px-24"
         >
           {/* HERO BACKGROUND */}
           <div className="z-10 absolute inset-0 overflow-clip">
             <Image
               src="/assets/imgs/bg-last.webp"
               alt="Background Image"
-              layout="fill"
-              objectFit="cover"
+              fill
               quality={100} // MAX 100
-              style={{ zIndex: -1 }}
+              style={{ zIndex: -1, objectFit: "cover" }}
             />
             <Image
               src="/assets/imgs/bg-medium 2.png"
               className="mix-blend-screen glitch-image"
               alt="Background Image"
-              layout="fill"
-              objectFit="cover"
+              fill
               quality={100} // MAX 100
-              style={{ zIndex: 0 }}
+              style={{ zIndex: 0, objectFit: "cover" }}
             />
             <Image
               src="/assets/imgs/bg-first.webp"
               alt="Background Image"
-              layout="fill"
-              objectFit="cover"
+              fill
               quality={100} // MAX 100
-              style={{ zIndex: 1 }}
+              style={{ zIndex: 1, objectFit: "cover" }}
             />
           </div>
 
@@ -55,13 +53,13 @@ export default function Home() {
                 experientes o aguarda. Juntos, vamos trazer à tona o melhor de
                 você.
               </h2>
-             
-             <DefaultBtn content="VER AULAS E TURMAS" width={'350px'} />
+
+              <DefaultBtn content="VER AULAS E TURMAS" width={"354px"} />
             </div>
           </div>
         </section>
 
-        <section id="classesSection" className="py-24">
+        <section id="classesSection" className="py-24 px-12 lg:px-24">
           <div className="flex flex-col gap-6 justify-center items-center">
             <h1 className="text-8xl font-kenyan">AULAS</h1>
             <h2 className="max-w-[55%] text-balance text-center text-md">
@@ -77,6 +75,98 @@ export default function Home() {
           </div>
 
           <CardsAula />
+        </section>
+
+        <section className="min-w-full h-[600px] py-24  bg-gradient-to-br from-zinc-800 to-black relative">
+          <div className="max-w-5xl mx-auto flex items-center h-full">
+            <div className="w-1/2 flex flex-col gap-5">
+              <h1 className="text-8xl font-kenyan text-white">
+                AULA EXPERIMENTAL
+              </h1>
+              <h2 className="  text-start text-balance text-md text-gray-200">
+                Você quer experimentar uma sessão de treinamento gratuita
+                conosco? Sem problemas! Basta vir durante nosso horário de
+                funcionamento regular e falar com nossa equipe na recepção.
+                <br />
+                <br />
+                Lá, iremos aconselhá-lo, encontrar uma data adequada juntos e
+                oferecer um tour pela academia.
+              </h2>
+            </div>
+            <div className="absolute w-full md:w-2/3 xl:w-1/2 h-[600px] top-0 right-0 md:mr-[-15vw] lg:mr-[-6vw] xl:mr-[0vw] 2xl:mr-[12vw] grayscale mask-rounded-3xl">
+              <Image
+                className="absolute top-[55px] right-[100px] opacity-30 mask-rounded-xl"
+                src="/assets/imgs/socador-box.png"
+                alt="Fight Image"
+                width={650}
+                height={150}
+                quality={100} // MAX 100
+                style={{ zIndex: 1, objectFit: "cover" }}
+              />
+              <Image
+                className="absolute top-[55px] right-[75px] opacity-60 mask-rounded-xl"
+                src="/assets/imgs/socador-box.png"
+                alt="Fight Image"
+                width={650}
+                height={150}
+                quality={100} // MAX 100
+                style={{ zIndex: 2, objectFit: "cover" }}
+              />
+              <Image
+                className="absolute top-[55px] right-[50px] mask-rounded-xl"
+                src="/assets/imgs/socador-box.png"
+                alt="Fight Image"
+                width={650}
+                height={150}
+                quality={100} // MAX 100
+                style={{ zIndex: 3, objectFit: "cover" }}
+              />
+
+              <Image
+                className="absolute top-[140px] right-[110px] "
+                src="/assets/imgs/saco-box.png"
+                alt="Fight Image"
+                width={450}
+                height={50}
+                quality={100} // MAX 100
+                style={{ zIndex: 4, objectFit: "cover" }}
+              />
+
+              <Image
+                className="absolute top-[0px] right-[155px]"
+                src="/assets/imgs/gym-chain.png"
+                alt="Fight Image"
+                width={150}
+                height={50}
+                quality={100} // MAX 100
+                style={{ zIndex: 1, objectFit: "cover" }}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="min-w-full h-screen py-24 relative">
+          <ContainerScroll
+            titleComponent={
+              <>
+                <h1 className="text-4xl font-semibold text-black dark:text-white">
+                  Unleash the power of <br />
+                  <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                    Scroll Animations
+                  </span>
+                </h1>
+              </>
+            }
+          >
+            <Image
+              src={`/assets/imgs/inter-section.webp`}
+              alt="hero"
+              height={720}
+              width={1400}
+              className="mx-auto object-cover h-full w-full object-left-top"
+              draggable={false}
+            />
+          </ContainerScroll>
         </section>
       </main>
     </>
--- src/components/defaultBtn.tsx
+++ src/components/defaultBtn.tsx
@@ -1,17 +1,24 @@
-import React, { ButtonHTMLAttributes, ReactHTMLElement, ReactNode } from "react";
+import React, {
+  ButtonHTMLAttributes,
+  ReactHTMLElement,
+  ReactNode,
+} from "react";
 
 interface DefaultBtnProps {
-    children?: ReactNode;
-    content: string;
-    width: string;
+  children?: ReactNode;
+  content: string;
+  width: string;
 }
 
-export default function DefaultBtn({content, width}: DefaultBtnProps) {
+export default function DefaultBtn({ content, width }: DefaultBtnProps) {
   return (
-    <button className={`relative inline-flex overflow-hidden p-[1px] focus:outline-none w-[${width}] `}>
+    <button
+      className={`relative inline-flex overflow-hidden p-[1px] focus:outline-none `}
+      style={{ width: width }}
+    >
       <span className="absolute inset-[-1000%] animate-spin-slow bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"></span>
       <span className=" button-glitch inline-flex h-full w-full cursor-pointer font-bold items-center justify-center bg-slate-950 px-3 py-1 text-md tracking-tight text-white backdrop-blur-3xl">
-       {content}
+        {content}
       </span>
     </button>
   );
